c34df10d6a6f63a4ceee6fce08f4ec3b6f11e889
Mustafa Morca <psychoplasma@gmail.com>
2018-11-21 16:12:44 +0900

    Add configurations for auto-deployment through truffle

--- migrations/2_spintoken.js
+++ migrations/2_spintoken.js
@@ -1,12 +1,45 @@
 const SpinToken = artifacts.require("./SpinToken.sol");
 
-// const WEI = 10**18;
-
 const name = "SPIN Protocol";
 const symbol = "SPIN";
 const decimals = 18;
 const initialSupply = 1250000000;
 
+// Local testnet deployer
 module.exports = function(deployer) {
-  //deployer.deploy(SpinToken, name, symbol, decimals, initialSupply);
+  // Leave this blank to run unit tests
 }
+
+/*************SPIN Token***************/
+// Mainnet
+// Mainnet deployment of SPIN Token contract
+// @see https://etherscan.io/token/0x???
+// const SPIN_TOKEN_ADDRESS = '';
+
+// Ropsten
+// Testnet deployment of SPIN Token contract
+// @see https://ropsten.etherscan.io/token/0xd3bac9db564f649b756acbce5cb24471d39b13b5
+// const SPIN_TOKEN_ADDRESS = '0xd3bac9db564f649b756acbce5cb24471d39b13b5';
+
+// Rinkeby
+// Testnet deployment of SPIN Token contract
+// @see https://rinkeby.etherscan.io/token/0xd3bac9db564f649b756acbce5cb24471d39b13b5
+// const SPIN_TOKEN_ADDRESS = '0xd3bac9db564f649b756acbce5cb24471d39b13b5';
+
+// Testnet Deployer
+// module.exports = (deployer) => {
+//   deployer.deploy(SpinToken, name, symbol, decimals, initialSupply)
+//     .then( _ => console.log('SPIN Token contract has been deployed successfully.'));
+// };
+
+
+// Notice that in mainnet deployments, we set a delay before resolving the deployer.deploy() fucntion
+// because, the transactions are not immediately available even if they are mined
+// at that specific time. Therefore we need to wait a while until the tx is available.
+// The delay period is not certain, set by trial!!!
+
+// Mainnet deployer of SPIN Token contract
+// module.exports = (deployer, network, accounts) => {
+//     deployer.deploy(SpinToken, accounts[0])
+//         .then( _ => console.log('SPIN Token contract has been deployed successfully.'));
+// };
